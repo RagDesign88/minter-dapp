bc907fb94b4396002424e3a93650d76e76a166fc
RAGMedia88 <rag.creativemedia@gmail.com>
2022-08-31 17:07:12 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x8D5Ab6D942A7c8Ae36D79770e18B62FD25E75422"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
