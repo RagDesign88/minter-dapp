340fd67df8a0812fee6a754db57a62a42d5f6a92
RAGMedia88 <rag.creativemedia@gmail.com>
2022-09-22 17:03:30 -0400

    remove p

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,6 @@ const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "This Is Crack Rabbit Collective";
 const welcome_h2 = "Connect your wallet 4 crack";
-const welcome_p = '';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
