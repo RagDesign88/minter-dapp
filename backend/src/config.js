a3b53c4649a35771380433d7094954357647cbce
RAGMedia88 <rag.creativemedia@gmail.com>
2022-10-02 14:15:11 -0400

    changes mint start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,15 +59,15 @@ const CONTRACT_SYMBOL = 'CRC';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x74bBb01c648d2D6b02653bcfC5fb3a6955e4DAcE';
 const TREASURY_ADDRESS = '0x74bBb01c648d2D6b02653bcfC5fb3a6955e4DAcE';
-const MAX_SUPPLY = 10050; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MAX_SUPPLY = 10000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 15; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-21T01:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-10-10T01:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-10-01T01:00:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x74bBb01c648d2D6b02653bcfC5fb3a6955e4DAcE"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
