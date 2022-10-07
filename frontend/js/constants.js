a0466ec9e71c6239f81dd0dcee3e4117ab163fc3
RAGMedia88 <rag.creativemedia@gmail.com>
2022-10-07 17:07:53 -0400

    alll the changes bitch

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,23 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0xEdAA8a5a266b20199F0b886503e327971F60ca8d"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Crack Rabbit Collective";
+const welcome_h2 = "Presenting the best NFT collection";
+const welcome_p = "Connect your wallet 4 crack & scroll down";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_presale_coming_soon = "These Crack Rabbits ain't easy to get ahold of. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "You ain't whitelisted for no pre-sale";
+const p_presale_mint_whitelisted = "You're on da pre-sale whitelist!";
+const p_presale_mint_already_minted = "Take it easy you've already got your whitelist mint";
+const p_public_mint = "No whitelist needed. Public minting is now open!";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Crack Rabbit";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
+const button_presale_already_minted = "Join The Crackmmunity";
 const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const mint_failed = "Minting failed. try again.";
+
